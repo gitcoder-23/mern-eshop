7ff469188781b894392942f980c9c492cf8cf8f9
gitcoder-23 <prabhatece21@gmail.com>
2021-03-16 13:22:39 +0530

    make it better

--- server.js
+++ server.js
@@ -1,50 +1,42 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const fileUpload = require('express-fileupload');
-const cookieParser = require('cookie-parser');
-// path setup for diployment production
-const path =require('path');
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const cookieParser = require('cookie-parser')
+const path = require('path')
 
 
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors());
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
 app.use(fileUpload({
-    useTempFiles: true,
-}));
-
+    useTempFiles: true
+}))
 
 // Routes
-app.use('/user', require('./routes/userRouter'));
-app.use('/api', require('./routes/categoryRouter'));
-app.use('/api', require('./routes/upload'));
-app.use('/api', require('./routes/productRouter'));
-
-// payment api
-app.use('/api', require('./routes/paymentRouter'));
+app.use('/user', require('./routes/userRouter'))
+app.use('/api', require('./routes/categoryRouter'))
+app.use('/api', require('./routes/upload'))
+app.use('/api', require('./routes/productRouter'))
+app.use('/api', require('./routes/paymentRouter'))
 
 
 
-
-
-// connect to mongoo
-const URI = process.env.MONGODB_URL;
+// Connect to mongodb
+const URI = process.env.MONGODB_URL
 mongoose.connect(URI, {
     useCreateIndex: true,
     useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
-}, err => {
+}, err =>{
     if(err) throw err;
-    console.log('Connected mongodb name "ecommerce"');
-});
+    console.log('Connected to MongoDB')
+})
 
-// path setup for diployment & production
-// rule of client & server start by one 
-if(process.env.NODE_END === 'production'){
+if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
@@ -52,14 +44,8 @@ if(process.env.NODE_END === 'production'){
 }
 
 
-// app.get('/', (req, res) => {
-//     res.json({
-//         msg: 'Node is running, its good'
-//     })
-// });
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-    console.log('Server is running on port->', PORT);
-});
-
+app.listen(PORT, () =>{
+    console.log('Server is running on port', PORT)
+})
